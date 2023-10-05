c30a5faa60ed226fcd1051d9c9c1f08dc472a44a
LevOzay <leventozay2003@gmail.com>
2023-10-04 21:32:56 -0400

    Made a pretty demo UI with a title, more buttons, and text field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Flo.CI Claim Submission Demo</h1>
+      <h2>
+        <Stack spacing={2} direction="row">
+          <Button variant="contained" color="success" sx={{m: 1}}>Submit claim</Button>
+          <Button variant="contained" color="success" sx={{m: 1}}>Retrieve claim</Button>
+        </Stack>
+      </h2>
+      <h3>
+        <TextField id="filled-basic" label="Please enter claim number" variant="filled" 
+        color="success"  sx={{m: 1, width: '42ch'}}/>
+      </h3>
+      <h4>
+      <TextField disabled id="filled-basic" label="Claim Number (from database)" variant="filled" 
+        color="success"  sx={{m: 1, width: '49ch'}}/>
+      </h4>
     </div>
   );
 }
