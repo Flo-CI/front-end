f1927ba76eef3098ce511ef6c9e6f901a1405e53
BruhSomeoneTookMySpicyUsername <geraldyywang@gmail.com>
2023-10-31 12:56:54 -0400

    Added claim dashboard, cards, refactored code - Created ClaimDashboardScreen, ClaimCard, Navbar, LoginScreen - Added tailwindcss, heroicons, react-router-dom dependencies - Refactored LoginScreen logic to its own file, so that App only focuses on routing

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { Grid } from "@mui/material";
+
+const HomeScreen = () => {
+  const base_url = "https://ciflo.azurewebsites.net/demo/claim?policyNumber=";
+  const [data, setData] = useState({});
+
+  // Define a state variable to store the user input
+  const [claimNumber, setClaimNumber] = useState("");
+
+  // Event handler to update the claimNumber state when the user types
+  const handleClaimNumberChange = (event) => {
+    setClaimNumber(event.target.value);
+  };
+
+  // Function that grabs info of user inputted claim number
+  const fetchInfo = () => {
+    // Calls GET Database API of specific claim number and saves it to data
+    const response = fetch(base_url + claimNumber, {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data));
+
+    try {
+      // Checks to see if the account found is correct
+      if (claimNumber === data["details"]["0"]["policyNumber"]) {
+        alert("The user id is " + data["details"]["0"]["id"]);
+      } else {
+        alert("The inputted policy number is not currently registered");
+      }
+    } catch (e) {}
+  };
+
+  // Calls POST Database API of user inputted claim number
+  const uploadData = () => {
+    fetch(base_url + claimNumber, {
+      method: "POST",
+      data: {
+        policyNumber: claimNumber,
+      },
+    });
+  };
+
+  return (
+    <div>
+      <Grid container justify="center" alignItems="center" direction="column">
+        <Grid item>
+          <h2 style={{ textAlign: "center" }}>Flo.CI Claim Submission Demo</h2>
+        </Grid>
+        <Grid item>
+          <h2>
+            <Stack
+              style={{ justifyContent: "center" }}
+              spacing={2}
+              direction="row"
+            >
+              <Button
+                variant="contained"
+                color="success"
+                onClick={uploadData}
+                sx={{ m: 1 }}
+              >
+                Add Claim Number
+              </Button>
+              <Button
+                variant="contained"
+                color="success"
+                onClick={fetchInfo}
+                sx={{ m: 1 }}
+              >
+                Check Claim Number
+              </Button>
+            </Stack>
+          </h2>
+        </Grid>
+        <Grid item>
+          <h2>
+            <TextField
+              id="filled-basic"
+              label="Please enter claim number"
+              variant="filled"
+              color="success"
+              onChange={handleClaimNumberChange}
+              sx={{ m: 1, width: "42ch" }}
+            />
+          </h2>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default HomeScreen;
