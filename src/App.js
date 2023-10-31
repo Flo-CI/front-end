af0d65d6ef72f39685d99a3a60595e7a0bfdb77e
Gregory G <gr.gismondi@gmail.com>
2023-10-31 09:34:08 -0400

    fixed double button call error. now data will only be checked once the database has returned

--- src/App.js
+++ src/App.js
@@ -1,50 +1,75 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import {Grid} from "@mui/material";
 
 function App() {
+    // Base URL for database calls
     const base_url = "https://ciflo.azurewebsites.net/demo/claim?policyNumber=";
-    const [data, setData] = useState({});
+    const [data, setData] = useState(null);
 
-    // Define a state variable to store the user input
-    const [claimNumber, setClaimNumber] = useState('');
+    // State variable that holds the user's inputted text and text when they click the log-in button
+    const [textboxValue, setTextboxValue] = useState('');
+    const [accountNumber, setAccountNumber] = useState('');
 
-    // Event handler to update the claimNumber state when the user types
-    const handleClaimNumberChange = (event) => {
-        setClaimNumber(event.target.value);
+    // Event handler to update the textboxValue state when the user types
+    const handleTextboxValueChange = (event) => {
+        setTextboxValue(event.target.value);
     };
+    // Event handler to update accountValue state when the user clicks the log-in button
+    const handleAccountNumberChange = () => {
+        setAccountNumber(textboxValue);
+    }
 
-    // Function that grabs info of user inputted claim number
-    const fetchInfo = () => {
-        // Calls GET Database API of specific claim number and saves it to data
-        const response = fetch(base_url + claimNumber, {method: "GET", headers: {
-                accept: 'application/json',
-            },})
-            .then((response) => response.json())
-            .then((data) => setData(data));
-
+    // Gets the returned info from the database and checks if it exists
+    const dataCheck = () => {
         try {
             // Checks to see if the account found is correct
-            if (claimNumber === data['details']['0']['policyNumber']) {
+            if (accountNumber === data['details']['0']['policyNumber']) {
                 alert("The user id is " + data['details']['0']['id']);
+            // Returns if account info is not correct
             } else {
-                alert("The inputted policy number is not currently registered")
+                alert("The inputted account number does not exist")
             }
 
         } catch (e) {
-
+            // Catch statement when no user statement is returned
+            alert("The inputted account number does not exist")
         }
     }
 
-    // Calls POST Database API of user inputted claim number
+    // Calls GET Database API of user inputted account number
+    // Occurs whenever the accountNumber state variable is changed
+    useEffect(() => {
+        const fetchData = async () => {
+            const fetchResult = await fetch(base_url + textboxValue, {
+                method: "GET", headers: {
+                    accept: 'application/json',
+                },
+            })
+            const newData = await fetchResult.json()
+            setData(newData)
+        }
+        if (textboxValue !== '') {
+            fetchData()
+        }
+    }, [accountNumber]);
+
+    // Checks account info whenever the data is returned from the database
+    useEffect(() => {
+        if (textboxValue !== ''){
+            dataCheck();
+        }
+    }, [data]);
+
+    // Calls POST Database API of user inputted account number
     const uploadData = () => {
-        fetch(base_url + claimNumber, {
+        fetch(base_url + accountNumber, {
             method: 'POST',
             data: {
-                policyNumber: claimNumber
+                policyNumber: accountNumber
             }
         })
     }
@@ -59,14 +84,14 @@ function App() {
                 <h2>
                     <Stack style={{justifyContent: 'center'}} spacing={2} direction="row">
                       <Button variant="contained" color="success" onClick={uploadData} sx={{m: 1}}>Add Claim Number</Button>
-                      <Button variant="contained" color="success" onClick={fetchInfo} sx={{m: 1}}>Check Claim Number</Button>
+                      <Button variant="contained" color="success" onClick={() => handleAccountNumberChange()} sx={{m: 1}}>Check Claim Number</Button>
                     </Stack>
                 </h2>
             </Grid>
             <Grid item>
                 <h2>
                     <TextField id="filled-basic" label="Please enter claim number" variant="filled"
-                    color="success" onChange={handleClaimNumberChange} sx={{m: 1, width: '42ch'}}/>
+                    color="success" onChange={handleTextboxValueChange} sx={{m: 1, width: '42ch'}}/>
                 </h2>
             </Grid>
         </Grid>
