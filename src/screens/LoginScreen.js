af5105cb9f97e22da05dafaac6a1629a4901bb7b
BruhSomeoneTookMySpicyUsername <geraldyywang@gmail.com>
2023-11-01 23:15:07 -0400

    Addresses issues #22, #23, #24, #25     - Updated ClaimDashboardScreen with "Dashboard" header and added new claim button     - Added durian logo     - Added background image     - Used state to toggle between login and dashboard screens     - Updated LoginScreen design to better match Figma     - Changed some colors in ClaimCard and ClaimDashboardScreen to increase readability

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,16 +4,26 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { Grid } from "@mui/material";
 
-function LoginScreen() {
+import background from "../assets/background.png";
+import flo from "../assets/Flo-CI_Icon.png";
+
+function LoginScreen({ handleAuthentication }) {
+  /* 
+  Valid logins:
+  Username, Password
+  123456, 52
+  123, 606
+  619, 305
+  **/
+
   // Base URL for database calls
   const base_url = "https://ciflo.azurewebsites.net/demo/claim?policyNumber=";
   const [data, setData] = useState(null);
 
   // State variable that holds the user's inputted text and text when they click the log-in button
-  const [usernameValue, setUsernameValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-  const [policyNumber, setPolicyNumber] = useState('');
-  const [authenticated, setAuthenticated] = useState(false);
+  const [usernameValue, setUsernameValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+  const [policyNumber, setPolicyNumber] = useState("");
 
   // Event handler to update the usernameValue state when the user types
   const handleUsernameValueChange = (event) => {
@@ -22,25 +32,25 @@ function LoginScreen() {
   // Event handler to update accountValue state when the user clicks the log-in button
   const handlePolicyNumberChange = () => {
     setPolicyNumber(usernameValue);
-  }
+  };
   const handlePasswordValueChange = (event) => {
     setPasswordValue(event.target.value);
-  }
+  };
 
   // Gets the returned info from the database and checks if it exists
   const dataCheck = () => {
     try {
       // Checks to see if the account found is correct
-      if (parseInt(passwordValue) === data['details']['0']['id']) {
-        alert("The policy number and password is valid");
-        setAuthenticated(true);
-      // Returns if account info is not correct
+      if (parseInt(passwordValue) === data["details"]["0"]["id"]) {
+        // alert("The policy number and password is valid");
+        handleAuthentication();
+        // Returns if account info is not correct
       } else {
-        alert("The password is not correct for the given policy number")
+        alert("The password is not correct for the given policy number");
       }
     } catch (e) {
       // Catch statement when no user statement is returned
-      alert("The inputted policy number does not exist")
+      alert("The inputted policy number does not exist");
     }
   };
 
@@ -49,22 +59,23 @@ function LoginScreen() {
   useEffect(() => {
     const fetchData = async () => {
       const fetchResult = await fetch(base_url + usernameValue, {
-        method: "GET", headers: {
-          accept: 'application/json',
+        method: "GET",
+        headers: {
+          accept: "application/json",
         },
-      })
-      const newData = await fetchResult.json()
-      setData(newData)
-      setPolicyNumber('');
-    }
-    if (policyNumber !== ''){
-      fetchData()
+      });
+      const newData = await fetchResult.json();
+      setData(newData);
+      setPolicyNumber("");
+    };
+    if (policyNumber !== "") {
+      fetchData();
     }
   }, [policyNumber]);
 
   // Checks account info whenever the data is returned from the database
   useEffect(() => {
-    if (usernameValue !== ''){
+    if (usernameValue !== "") {
       dataCheck();
     }
   }, [data]);
@@ -72,41 +83,83 @@ function LoginScreen() {
   // Calls POST Database API of user inputted account number
   const uploadData = () => {
     fetch(base_url + policyNumber, {
-      method: 'POST',
+      method: "POST",
       data: {
-        policyNumber: policyNumber
-      }
-    })
-  }
+        policyNumber: policyNumber,
+      },
+    });
+  };
 
   return (
-      <div>
+    <div className="flex flex-row justify-center items-center h-screen w-screen">
+      <div className="flex flex-auto items-center justify-center h-screen">
+        <img
+          src={background}
+          alt="background"
+          className="h-screen w-full blur-sm hue-rotate-30"
+        />
+        <div className="flex justify-center items-center z-10 p-12 absolute">
+          <h1 className="text-8xl font-bold text-green-900">Sedurian Canada</h1>
+          <img src={flo} alt="FloCI Logo" className="h-40" />
+        </div>
+      </div>
+
+      {/* Login Components */}
+      <div className="flex flex-col bg-gray-50 items-center justify-center h-screen p-8 w-4/12">
+        <h1 className="text-4xl font-bold">Flo.CI Login</h1>
+        {/* Login text boxes and buttons */}
         <Grid container justify="center" alignItems="center" direction="column">
           <Grid item>
-            <h2 style={{textAlign: "center", marginTop: 50, fontSize: 20, fontWeight: "bold"}}>Flo.CI Claim Submission Demo</h2>
+            <h2>
+              <TextField
+                id="filled-basic"
+                label="Please enter policy number"
+                variant="filled"
+                color="success"
+                onChange={handleUsernameValueChange}
+                sx={{ m: 3, width: "42ch" }}
+              />
+            </h2>
           </Grid>
           <Grid item>
             <h2>
-              <Stack style={{justifyContent: 'center'}} spacing={2} direction="row" sx={{m: 2}}>
-                <Button variant="contained" color="success" onClick={uploadData}>Sign-Up</Button>
-                <Button variant="contained" color="success" onClick={() => handlePolicyNumberChange()}>Log-In</Button>
+              <TextField
+                id="filled-basic"
+                label="Please enter policy password"
+                variant="filled"
+                color="success"
+                onChange={handlePasswordValueChange}
+                sx={{ m: 1, width: "42ch" }}
+              />
+            </h2>
+          </Grid>
+          <Grid item>
+            <h2>
+              <Stack
+                style={{ justifyContent: "center" }}
+                spacing={2}
+                direction="row"
+                sx={{ m: 2 }}
+              >
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={() => handlePolicyNumberChange()}
+                >
+                  Log-In
+                </Button>
+                <Button
+                  variant="contained"
+                  color="success"
+                  onClick={uploadData}
+                >
+                  Sign-Up
+                </Button>
               </Stack>
             </h2>
           </Grid>
-          <Grid item>
-            <h2>
-              <TextField id="filled-basic" label="Please enter policy number" variant="filled"
-                         color="success" onChange={handleUsernameValueChange} sx={{m: 3, width: '42ch'}}/>
-            </h2>
-          </Grid>
-          <Grid item>
-            <h2>
-              <TextField id="filled-basic" label="Please enter policy password" variant="filled"
-                         color="success" onChange={handlePasswordValueChange} sx={{m: 1, width: '42ch'}}/>
-            </h2>
-          </Grid>
         </Grid>
-      </Grid>
+      </div>
     </div>
   );
 }
