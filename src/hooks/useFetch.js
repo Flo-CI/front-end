fd7ddf698c09a1344f9a4e4b95c92ec08fb51808
bruhspicy <geraldyywang@gmail.com>
2023-12-02 23:37:35 -0500

    useFetch hook, securian_logo updated for tab

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        // Get the resource, return it as a JSON object
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
+        return res.json();
+      })
+      .then((d) => {
+        // Setting the data
+        setData(d);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        // Catching errors
+        // Set this if so that we are not updating state when it is an abort error
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          console.log(err.message);
+          setIsPending(false);
+          setError(err.message);
+        }
+      });
+
+    // Stop the fetch if we quickly switch between tabs
+    return () => {
+      // When we abort, it throws an error
+      abortCont.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
