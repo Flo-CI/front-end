bb0912a428cef94d25a919b8cf5e5ce0f1874397
LevOzay <leventozay2003@gmail.com>
2023-12-04 15:18:13 -0500

    Created sample FileCard

--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -1,7 +1,22 @@
 import React, { useContext } from "react";
 import { DarkModeContext } from "../DarkModeContext";
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-export default functions FileCard({
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export default function FileCard({
     fileName,
     fileDate,
     fileStatus,
@@ -11,5 +26,31 @@ export default functions FileCard({
         ? 'bg-green-500 text-white rounded-t-xl rou'
         : 'bg-green-300 rounded-t-xl rou';
     return (
+    <div>
+    <div className={color}>
+        <h1 className="px-4 py-4 font-bold text-xl">{fileName}</h1>
+    </div>
 
+    {/* Claim status, claim number, date filed */}
+    <div className={darkMode ? 'dark' : 'light'}>
+        <h2 className="px-4 py-2 flex">
+            Status:{" "}
+            <p className="px-1 font-semibold">{fileStatus}</p>
+        </h2>
+        <h2 className="px-4 py-2 flex">
+            Last Updated:{" "}
+            <p className="px-1 font-semibold">{fileDate}</p>
+        </h2>
+        <h2>
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                Upload file
+                <VisuallyHiddenInput type="file" />
+            </Button>
+            <Button variant="contained">View File</Button>
+        </h2>
+
+
+    </div>
+    </div>
+    );
 }
