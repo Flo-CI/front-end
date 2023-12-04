e41b86b26425026fbe6db27b62fa7c7af309d273
ansh <ansh30704@gmail.com>
2023-12-03 22:10:11 -0500

    same for notfoundscreen

--- src/__test__/NotFoundScreen.test.js
+++ src/__test__/NotFoundScreen.test.js
@@ -1,12 +1,15 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import NotFoundScreen from '../screens/NotFoundScreen';
-
+import { Provider as ReduxProvider } from 'react-redux';
+import store from '../state/store';
 test('Renders NotFoundScreen with 404 message and link', () => {
   const { getByText } = render(
+    <ReduxProvider store={store}>
     <MemoryRouter>
       <NotFoundScreen />
     </MemoryRouter>
+    </ReduxProvider>
   );
 
   const notFoundMessage = getByText('404 Not Found');
