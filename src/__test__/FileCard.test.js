4e4a14d00c58face699bd54c9650ea32f69354cd
ansh <ansh30704@gmail.com>
2023-12-04 23:49:09 -0500

    Fixed formatting for FIleCard

--- src/__test__/FileCard.test.js
+++ src/__test__/FileCard.test.js
@@ -6,48 +6,48 @@ import { DarkModeProvider } from '../DarkModeContext'; // Adjust the import path
 
 
 describe('FileCard Component', () => {
-  it('renders FileCard component with correct file details in dark mode', () => {
-    const fileName = 'TestFile';
-    const fileDate = '2023-12-03';
-    const fileStatus = 'Pending';
+    it('renders FileCard component with correct file details in dark mode', () => {
+        const fileName = 'TestFile';
+        const fileDate = '2023-12-03';
+        const fileStatus = 'Pending';
 
-    const onClickMock = jest.fn();
-    const { getByText } = render(
-        <DarkModeProvider>
-            <FileCard
-            fileName={fileName}
-            fileDate={fileDate}
-            fileStatus={fileStatus}
-            onClick={onClickMock}
-            />
-        </DarkModeProvider>
-    );
+        const onClickMock = jest.fn();
+        const { getByText } = render(
+            <DarkModeProvider>
+                <FileCard
+                    fileName={fileName}
+                    fileDate={fileDate}
+                    fileStatus={fileStatus}
+                    onClick={onClickMock}
+                />
+            </DarkModeProvider>
+        );
 
-    const fileNameElement = getByText(fileName);
-    expect(fileNameElement).toBeInTheDocument();
+        const fileNameElement = getByText(fileName);
+        expect(fileNameElement).toBeInTheDocument();
 
-    // Add more assertions based on your component structure and test requirements
-    // For instance, you can check for the presence of other elements based on fileStatus, fileDate, etc.
-  });
+        // Add more assertions based on your component structure and test requirements
+        // For instance, you can check for the presence of other elements based on fileStatus, fileDate, etc.
+    });
 
-  it('fires onClick function when FileCard is clicked', () => {
-    const onClickMock = jest.fn();
-    const { container } = render(
-        <DarkModeProvider>
-            <FileCard
-            fileName="TestFile"
-            fileDate="2023-12-03"
-            fileStatus="Pending"
-            onClick={onClickMock}
-            />
-        </DarkModeProvider>
-    );
+    it('fires onClick function when FileCard is clicked', () => {
+        const onClickMock = jest.fn();
+        const { container } = render(
+            <DarkModeProvider>
+                <FileCard
+                    fileName="TestFile"
+                    fileDate="2023-12-03"
+                    fileStatus="Pending"
+                    onClick={onClickMock}
+                />
+            </DarkModeProvider>
+        );
 
-    const fileCard = container.firstChild; // Assuming the first child is the clickable area
-    fireEvent.click(fileCard);
+        const fileCard = container.firstChild; // Assuming the first child is the clickable area
+        fireEvent.click(fileCard);
 
-    expect(onClickMock).toHaveBeenCalled();
-  });
+        expect(onClickMock).toHaveBeenCalled();
+    });
 
-  // Add more tests for different scenarios as needed
+    // Add more tests for different scenarios as needed
 });
