b9029255d2677f716b43f767af57a6710f29d070
ansh <ansh30704@gmail.com>
2023-12-06 12:49:49 -0500

    plus one last

--- src/__test__/ClaimFilesButton.test.js
+++ src/__test__/ClaimFilesButton.test.js
@@ -4,13 +4,8 @@ import { MemoryRouter } from 'react-router-dom';
 import ClaimFilesButton from '../components/ClaimFilesButton'; // Adjust the import path as needed
 import { DarkModeContext, DarkModeProvider } from '../DarkModeContext'; // Adjust the import path as needed
 import { BrowserRouter as Router } from 'react-router-dom';
-
-// Test to check rendering of ClaimFilesButton in light mode
 test('ClaimFilesButton renders correctly in light mode', () => {
-  // Mock function to simulate button click
   const onClickMock = jest.fn();
-
-  // Render ClaimFilesButton within a light mode context (DarkModeContext with darkMode: false)
   const { getByText } = render(
     <DarkModeContext.Provider value={{ darkMode: false }}>
       <MemoryRouter>
@@ -19,22 +14,16 @@ test('ClaimFilesButton renders correctly in light mode', () => {
     </DarkModeContext.Provider>
   );
 
-  // Check if the 'Create Claim' button is present and has a specific class in light mode
   const createClaimButton = getByText('Create Claim');
   expect(createClaimButton).toBeInTheDocument();
-  expect(createClaimButton).toHaveClass('bg-green-300');
+  expect(createClaimButton).toHaveClass('bg-green-300'); // Assuming default light mode class
 
-  // Simulate a click on the button and verify if the onClickMock function was called
   fireEvent.click(createClaimButton);
   expect(onClickMock).toHaveBeenCalled();
 });
 
-// Test to check rendering of ClaimFilesButton in dark mode
 test('ClaimFilesButton renders correctly in dark mode', () => {
-  // Mock function to simulate button click
   const onClickMock = jest.fn();
-
-  // Render ClaimFilesButton within a dark mode context (DarkModeContext with darkMode: true)
   const { getByText } = render(
     <DarkModeContext.Provider value={{ darkMode: true }}>
       <MemoryRouter>
@@ -43,12 +32,26 @@ test('ClaimFilesButton renders correctly in dark mode', () => {
     </DarkModeContext.Provider>
   );
 
-  // Check if the 'Create Claim' button is present and has a different class in dark mode
   const createClaimButton = getByText('Create Claim');
   expect(createClaimButton).toBeInTheDocument();
-  expect(createClaimButton).toHaveClass('bg-green-500');
+  expect(createClaimButton).toHaveClass('bg-green-500'); // Assuming dark mode class
 
-  // Simulate a click on the button and verify if the onClickMock function was called
   fireEvent.click(createClaimButton);
   expect(onClickMock).toHaveBeenCalled();
 });
+
+test('calls onClick function when button is clicked', () => {
+    const onClickMock = jest.fn();
+    const { getByText } = render(
+        <DarkModeProvider>
+      <Router>
+        <ClaimFilesButton onClick={onClickMock} />
+      </Router>
+      </DarkModeProvider>
+    );
+
+    const createClaimButton = getByText('Create Claim');
+    fireEvent.click(createClaimButton);
+
+    expect(onClickMock).toHaveBeenCalled();
+  });
